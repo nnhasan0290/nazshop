9e10e47413997a0e76fa1c15727f0be418ffb751
Nazmul Hasan <nazmulhasan02901@gmail.com>
2023-09-10 11:38:36 +0000

    fix: header dropdown css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,18 +6,36 @@ import Dropdown from "../common/Dropdown";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center my-2 border-red-500">
+    <div className="flex justify-between items-center my-2 relative">
       <h1 className="font-bold text-2xl cursor-pointer">NAZSHOP</h1>
-      <div className="flex gap-4">
+      <div className="flex gap-4 items-stretch">
         <SearchForm />
-        <div className="flex gap-4 items-center">
-          <TfiWorld size={23} className="cursor-pointer" />
-          <AiOutlineUser size={25} className={"cursor-pointer"} />
-          <div className="relative cursor-pointer group">
+        <div className="flex gap-4">
+          <div className="relative cursor-pointer group flex items-center">
             <AiOutlineShopping size={28} />
-            <span className="absolute top-0 right-0 bg-black text-white  text-xs h-[15px] w-[15px] rounded-md flex items-center justify-center translate-x-[50%] translate-y-[-50%]">
+            <span className="absolute top-[10px] right-0 bg-black text-white  text-xs h-[15px] w-[15px] rounded-md flex items-center justify-center translate-x-[50%] translate-y-[-50%]">
               3
             </span>
+            <Dropdown>
+              <h2>option one</h2>
+              <h2>option one</h2>
+              <h2>option one</h2>
+              <h2>option one</h2>
+            </Dropdown>
+          </div>
+          <div className="relative cursor-pointer group flex items-center">
+            <TfiWorld size={23} className="cursor-pointer" />
+
+            <Dropdown>
+              <h2>option one</h2>
+              <h2>option one</h2>
+              <h2>option one</h2>
+              <h2>option one</h2>
+            </Dropdown>
+          </div>
+          <div className="relative cursor-pointer group flex items-center">
+            <AiOutlineUser size={25} className={"cursor-pointer"} />
+
             <Dropdown>
               <h2>option one</h2>
               <h2>option one</h2>
--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -1,6 +1,6 @@
 const Dropdown = ({ children }: any) => {
   return (
-    <div className="hidden group-hover:block bg-white p-3 top-[100%] right-0 border  min-w-[200px] absolute">
+    <div className="hidden group-hover:block rounded-sm shadow-lg bg-white p-3 top-[100%] border right-0 min-w-[200px] absolute">
       {children}
     </div>
   );
