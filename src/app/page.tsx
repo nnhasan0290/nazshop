8b276a1a9fe01b1b6fbea3a6d1190c7c4d626872
Nazmul Hasan <nazmulhasan02901@gmail.com>
2023-09-11 10:30:28 +0000

    fix: css issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <>
       <Header />
-    </div>
+    </>
   );
 }
