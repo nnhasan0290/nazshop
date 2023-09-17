5bf26d7eeb7127398fa66042f35b89d63bc6d4b3
Nazmul Hasan <nazmulhasan02901@gmail.com>
2023-09-17 13:46:41 +0000

    feat: dropdown top arrow

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -1,6 +1,6 @@
 const Dropdown = ({ children }: any) => {
   return (
-    <div className="hidden group-hover:block rounded-sm shadow-lg bg-white p-3 top-[100%] border right-0 min-w-[200px] absolute z-10 animate-drop origin-top">
+    <div className="hidden group-hover:block rounded-sm shadow-lg bg-white p-3 top-[100%] border right-0 min-w-[200px] absolute z-10 animate-drop origin-top before:content-[''] before:absolute before:h-[15px] before:w-[15px] before:border-t before:border-l before:top-0 before:right-1 before:bg-white before:translate-y-[-50%] before:rotate-45">
       {children}
     </div>
   );
